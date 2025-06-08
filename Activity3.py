41ba867307fd29b7d779926be8ee26b383eb65a3
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-06-08 16:36:54 +1000

    New3

---
+++ Activity3.py
@@ -0,0 +1,7 @@
+maths=int(input("enter the marks in maths"))
+sports=int(input("enter the marks in sports"))
+pdh=int(input("enter the marks in pdh"))
+spelling=int(input("enter the marks in spelling"))
+sum=maths+sports+pdh+spelling
+percentage=(sum/400)*100
+print("your percentage is",percentage)
